0d5754f08f937fc46c57e899ca65005b643ba84a
Pawel <pawelkrosny95@gmail.com>
2017-11-24 18:36:12 +0100

    main

---
+++ backend/tablefootball/src/main/java/com/chill/table/football/TableFootball.java
@@ -0,0 +1,12 @@
+package com.chill.table.football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableFootball {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TableFootball.class);
+    }
+}
