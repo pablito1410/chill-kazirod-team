0683663763be466f21a3ca1017a343f140f19dd0
pustypawel <pustypawel@gmail.com>
2017-11-24 17:52:52 +0100

    init

---
+++ backend/table-football/src/main/java/chillout/hackathon/tablefootball/TableFootballApplication.java
@@ -0,0 +1,12 @@
+package chillout.hackathon.tablefootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableFootballApplication.class, args);
+	}
+}
