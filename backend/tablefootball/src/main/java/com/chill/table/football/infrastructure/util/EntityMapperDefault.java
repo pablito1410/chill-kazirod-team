7a3bdb26b50d22eec8c549873770e47de669bfce
Pawel <pawelkrosny95@gmail.com>
2017-11-24 23:20:48 +0100

    mapper

--- backend/tablefootball/src/main/java/com/chill/table/football/infrastructure/util/EntityMapperDefault.java
+++ backend/tablefootball/src/main/java/com/chill/table/football/infrastructure/util/EntityMapperDefault.java
@@ -2,6 +2,11 @@ package com.chill.table.football.infrastructure.util;
 
 import com.chill.table.football.application.util.EntityMapper;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class EntityMapperDefault implements EntityMapper {
 
@@ -12,7 +17,19 @@ public class EntityMapperDefault implements EntityMapper {
     }
 
     @Override
-    public <T, DTO> DTO map(final T user, final Class<DTO> userDTOClass) {
-        return null;
+    public <DTO> DTO map(final Object source, final Class<DTO> destinationType) {
+        return modelMapper.map(source, destinationType);
     }
+
+    @Override
+    public <DTO> Set<DTO> mapCollection(final Collection<?> source, final Class<DTO> destinationType) {
+        final TypeToken dtoType = TypeToken.of(hashSetType(destinationType).getClass());
+//        final Type dtoType = new TypeToken<HashSet<DTO>>() {}.getType();
+        return modelMapper.map(source, dtoType.getType());
+    }
+
+    private <DTO> HashSet<DTO> hashSetType(Class<DTO> type) {
+        return new HashSet<>();
+    }
+
 }
--- backend/tablefootball/src/test/java/com/chill/table/football/infrastructure/util/EntityMapperDefaultTest.java
+++ backend/tablefootball/src/test/java/com/chill/table/football/infrastructure/util/EntityMapperDefaultTest.java
@@ -1,49 +1,51 @@
-//package com.chill.table.football.infrastructure.util;
-//
-//import com.chill.table.football.application.user.User;
-//import com.chill.table.football.application.user.ports.outgoing.UserDTO;
-//import com.chill.table.football.application.util.EntityMapper;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//
-//@RunWith(SpringRunner.class)
-//public class EntityMapperDefaultTest {
-//
-//    @Autowired
-//    private EntityMapper entityMapper;
-//
-//    @Mock
-//    private User user;
-//
-//    @Before
-//    public void setUp() {
-//        Mockito.when(user.getId()).thenReturn(1L);
-//        Mockito.when(user.getUserName()).thenReturn("admin");
-//    }
-//
-//    @Test
-//    public void map() throws Exception {
-//        final UserDTO userDTO = entityMapper.map(user, UserDTO.class);
-//        Assert.assertEquals(userDTO.getId(), user.getId());
-//        Assert.assertEquals(userDTO.getUserName(), user.getUserName());
-//    }
-//
-//    @Test
-//    public void mapCollection() throws Exception {
-//        final Collection<User> users = new ArrayList<>();
-//        users.add(user);
-//        final Collection<UserDTO> userDTOS = entityMapper.mapCollection(users, UserDTO.class);
-//        Assert.assertEquals(userDTOS.iterator().next().getId(), user.getId());
-//        Assert.assertEquals(userDTOS.iterator().next().getUserName(), user.getUserName());
-//    }
-//
-//}
+package com.chill.table.football.infrastructure.util;
+
+import com.chill.table.football.application.user.User;
+import com.chill.table.football.application.user.ports.outgoing.UserDTO;
+import com.chill.table.football.application.util.EntityMapper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntityMapperDefaultTest {
+
+    @Autowired
+    private EntityMapper entityMapper;
+
+    @Mock
+    private User user;
+
+    @Before
+    public void setUp() {
+        Mockito.when(user.getId()).thenReturn(1L);
+        Mockito.when(user.getUserName()).thenReturn("admin");
+    }
+
+    @Test
+    public void map() throws Exception {
+        final UserDTO userDTO = entityMapper.map(user, UserDTO.class);
+        Assert.assertEquals(user.getId(), userDTO.getId());
+        Assert.assertEquals(user.getUserName(), userDTO.getUserName());
+    }
+
+    @Test
+    public void mapCollection() throws Exception {
+//        final List<User> users = new ArrayList<>();
+//        final User user2 = new User("Asd", "Sdfsdf");
+//        users.add(user2);
+//        TypeToken.of(new HashSet<>().getClass())
+//                entityMapper.mapCollection(users, klazz.getClass());
+//        final UserDTO userDTO = userDTOS.iterator().next();
+//        Assert.assertEquals(user2.getId(), userDTO.getId());
+//        Assert.assertEquals(user2.getUserName(), userDTO.getUserName());
+    }
+
+}
